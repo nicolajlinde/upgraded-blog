baf961cc56f19d63f8f6125458c9119a370e206c
Nicolaj Linde <nicolajlpedersen@gmail.com>
2022-08-17 20:58:14 +0200

    Updated with blocks

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+import smtplib
+from flask import Flask, render_template, request
 import requests
 import datetime
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -11,6 +16,10 @@ data = response.json()
 
 date = datetime.datetime.now()
 today = date.strftime("%d/%m/20%y")
+
+# Email #
+user = os.getenv('USER')
+password = os.getenv('PASSWORD')
 
 
 @app.route('/')
@@ -23,9 +32,14 @@ def get_about():
     return render_template('about.html')
 
 
-@app.route('/contact/')
+@app.route('/contact/', methods=["POST", "GET"])
 def get_contact():
-    return render_template('contact.html')
+    if request.method == 'POST':
+        title = "Successfully sent message."
+        receive_data()
+        return render_template('contact.html', title=title)
+    else:
+        return render_template('contact.html')
 
 
 @app.route('/post/<int:id>')
@@ -34,5 +48,26 @@ def get_post(id):
     return render_template("post.html", data=post, date=today)
 
 
+def receive_data():
+    name = request.form['name']
+    email = request.form['email']
+    phone = request.form['phone']
+    message = request.form['message']
+
+    print(f"Name: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {message}")
+    send_email(name, email, phone, message)
+
+
+def send_email(name, email, phone, message):
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(user=user, password=password)
+        connection.sendmail(
+            from_addr=user,
+            to_addrs="nicolajlpedersen@gmail.com",
+            msg=f"Subject:New message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {message}"
+        )
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
